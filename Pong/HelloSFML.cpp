078fca08884ed3ae0ead1e81cb1c24ec12975808
Frank <Fridout106@gmail.com>
2018-03-08 22:08:05 -0500

    Add project files.

---
+++ Pong/HelloSFML.cpp
@@ -0,0 +1,186 @@
+/*
+* Simple Pong clone
+* Athuor: Andrew Ridout
+*/
+
+#include "stdafx.h"
+#include "SFML/Graphics.hpp"
+
+using namespace sf;
+
+int main()
+{
+	Vector2f windowDimensions;
+	windowDimensions.x = 900;
+	windowDimensions.y = 650;
+
+	VideoMode vm(windowDimensions.x, windowDimensions.y);
+
+	RenderWindow window(vm, "Pong");
+
+	const float paddleMovement = 0.075;
+
+	Vector2f playerDimensions;
+	playerDimensions.x = 5;
+	playerDimensions.y = 100;
+
+	RectangleShape player(playerDimensions);
+	const int playerXPosition = 15;
+	float playerYPosition = windowDimensions.y / 2 - (player.getSize().y / 2);
+
+	Vector2f computerDimensions;
+	computerDimensions.x = 5;
+	computerDimensions.y = 100;
+
+	RectangleShape computer(computerDimensions);
+	const int computerXPosition = windowDimensions.x - (playerXPosition + computerDimensions.x);
+	float computerYPosition = windowDimensions.y / 2 - (computerDimensions.y / 2);
+
+	const int ballRadius = 8;
+	const int ballPoints = 30;
+	const float ballMovement = 0.05;
+	CircleShape ball(ballRadius, ballPoints);
+	ball.setOrigin(ballRadius, ballRadius);
+	float ballXPosition = windowDimensions.x / 2;
+	float ballYPosition = windowDimensions.y / 2;
+
+	enum class BallXDirection { LEFT, RIGHT };
+	enum class BallYDirection { UP, DOWN };
+	BallXDirection ballXDirection = BallXDirection::LEFT;
+	BallYDirection ballYDirection = BallYDirection::UP;
+
+	while (window.isOpen())
+	{
+		/*
+		************
+		Handle input
+		************
+		*/
+		if (Keyboard::isKeyPressed(Keyboard::Up) && player.getPosition().y >= 0)
+		{
+			playerYPosition -= paddleMovement;
+		}
+
+		if ((Keyboard::isKeyPressed(Keyboard::Down)) && (player.getPosition().y <= (windowDimensions.y - playerDimensions.y)))
+		{
+			playerYPosition += paddleMovement;
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		/*
+		****************
+		UPDATE THE FRAME
+		****************
+		*/
+
+		player.setPosition(playerXPosition, playerYPosition);
+		computer.setPosition(computerXPosition, computerYPosition);
+		ball.setPosition(ballXPosition, ballYPosition);
+
+		if (ballXDirection == BallXDirection::LEFT)
+		{
+			ballXPosition -= ballMovement;
+		}
+
+		if (ballXDirection == BallXDirection::RIGHT)
+		{
+			ballXPosition += ballMovement;
+		}
+
+		if (ballYDirection == BallYDirection::DOWN)
+		{
+			ballYPosition += ballMovement;
+		}
+
+		if (ballYDirection == BallYDirection::UP)
+		{
+			ballYPosition -= ballMovement;
+		}
+
+		// Hitting player paddle
+		if (ball.getPosition().y >= player.getPosition().y && 
+			ball.getPosition().y <= (player.getPosition().y + playerDimensions.y) &&
+			(ball.getPosition().x - ballRadius) <= (player.getPosition().x + playerDimensions.x) &&
+			ballYDirection == BallYDirection::DOWN)
+		{
+			ballXDirection = BallXDirection::RIGHT;
+		}
+
+		if (ball.getPosition().y >= player.getPosition().y &&
+			ball.getPosition().y <= (player.getPosition().y + playerDimensions.y) &&
+			(ball.getPosition().x - ballRadius) <= (player.getPosition().x + playerDimensions.x) &&
+			ballYDirection == BallYDirection::UP)
+		{
+			ballXDirection = BallXDirection::RIGHT;
+		}
+
+		// Hitting computer paddle
+		if (ball.getPosition().y >= computer.getPosition().y && 
+			ball.getPosition().y <= (computer.getPosition().y + computerDimensions.y) &&
+			(ball.getPosition().x + ballRadius) >= computer.getPosition().x &&
+			ballYDirection == BallYDirection::DOWN)
+		{
+			ballXDirection = BallXDirection::LEFT;
+		}
+
+		if (ball.getPosition().y >= computer.getPosition().y &&
+			ball.getPosition().y <= (computer.getPosition().y + computerDimensions.y) &&
+			(ball.getPosition().x + ballRadius) >= computer.getPosition().x &&
+			ballYDirection == BallYDirection::UP)
+		{
+			ballXDirection = BallXDirection::LEFT;
+		}
+
+		// Hitting top of screen
+		if ((ball.getPosition().y - ballRadius) <= 0)
+		{
+			ballYDirection = BallYDirection::DOWN;
+		}
+
+		if ((ball.getPosition().y - ballRadius) <= 0)
+		{
+			ballYDirection = BallYDirection::DOWN;
+		}
+
+		// Hitting bottom of screen
+		if ((ball.getPosition().y + ballRadius) >= windowDimensions.y)
+		{
+			ballYDirection = BallYDirection::UP;
+		}
+
+		if ((ball.getPosition().y + ballRadius) >= windowDimensions.y)
+		{
+			ballYDirection = BallYDirection::UP;
+		}
+
+		// player AI
+		if (ballYDirection == BallYDirection::UP && computer.getPosition().y >= 0)
+		{
+			computerYPosition -= paddleMovement;
+		}
+
+		if (ballYDirection == BallYDirection::DOWN && (computer.getPosition().y <= (windowDimensions.y - computerDimensions.y)))
+		{
+			computerYPosition += paddleMovement;
+		}
+
+		/*
+		**************
+		Draw the scene
+		**************
+		*/
+
+		window.clear();
+		window.draw(player);
+		window.draw(computer);
+		window.draw(ball);
+		window.display();
+	}
+
+    return 0;
+}
+
